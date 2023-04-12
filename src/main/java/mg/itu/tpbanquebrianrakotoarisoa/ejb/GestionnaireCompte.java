1bb762f42057ee8787a50e5b3b40c0b524d0af10
Brian <brianrakotoarisoa@gmail.com>
2023-04-12 21:29:40 +0300

    Écriture d'un EJB session stateless GestionnaireCompte

---
+++ src/main/java/mg/itu/tpbanquebrianrakotoarisoa/ejb/GestionnaireCompte.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.tpbanquebrianrakotoarisoa.ejb;
+
+import jakarta.annotation.sql.DataSourceDefinition;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import java.util.List;
+import mg.itu.tpbanquebrianrakotoarisoa.entities.CompteBancaire;
+
+/**
+ *
+ * @author Brian Rakotoarisoa
+ */
+@DataSourceDefinition (
+    className="com.mysql.cj.jdbc.MysqlDataSource",
+    name="java:app/jdbc/banque",
+    serverName="localhost",
+    portNumber=3306,
+    user="root",
+    password="12345", 
+    databaseName="banque",
+    properties = {
+      "useSSL=false",
+      "allowPublicKeyRetrieval=true"
+    }
+)
+@Stateless
+public class GestionnaireCompte {
+    @PersistenceContext(unitName = "banquePU")
+    private EntityManager em;
+    
+    public void creerCompte(CompteBancaire c) {
+        em.persist(c);
+    }
+    
+    public List<CompteBancaire> getAllComptes() {
+        TypedQuery<CompteBancaire> query = em.createNamedQuery("CompteBancaire.findAll", CompteBancaire.class);
+        return query.getResultList();
+    }
+}
